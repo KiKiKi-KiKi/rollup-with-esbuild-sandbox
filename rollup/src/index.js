e73fd54ae34efdb0a2362af2c15871b6167d1e20
KiKiKi <chaika.design@gmail.com>
2022-08-04 15:47:02 +0900

    Bundle jQuery with Rollup

--- rollup/src/index.js
+++ rollup/src/index.js
@@ -1,7 +1,15 @@
+import $ from './lib/jquery.esm';
 import { sum } from './add';
 
 const a = sum(1, 2);
 console.log(a);
+
+$(() => {
+  const btn = $('.myButton');
+  btn.on('click', () => {
+    console.log('clicked!');
+  });
+});
 
 // iife だと return a になる
 export default a;
---
+++ rollup/src/lib/jquery.esm.js
@@ -0,0 +1,3 @@
+// cf https://qiita.com/yuto-ono/items/64b4dc4b3271cc2f5b51
+import 'jquery';
+export default window.jQuery;
