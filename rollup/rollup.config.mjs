e9deb3bcc76da2a6983f86f0057cb05714a0d738
KiKiKi <chaika.design@gmail.com>
2022-08-05 15:54:29 +0900

    :sparkles: Minify

--- rollup/rollup.config.mjs
+++ rollup/rollup.config.mjs
@@ -2,15 +2,23 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { babel } from '@rollup/plugin-babel';
 // not working with ESM and CJS
 import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
 
 const config = {
   // entrypoint
   input: './src/index.js',
-  output: {
-    file: './build/dist.js',
-    // esm, cjs, amd, system, iife, umd
-    format: 'iife',
-  },
+  output: [
+    {
+      file: './build/dist.js',
+      // esm, cjs, amd, system, iife, umd
+      format: 'iife',
+    },
+    {
+      file: './build/dist.min.js',
+      format: 'iife',
+      plugins: [terser()],
+    },
+  ],
   plugins: [commonjs(), nodeResolve(), babel({ babelHelpers: 'bundled' })],
 };
 
