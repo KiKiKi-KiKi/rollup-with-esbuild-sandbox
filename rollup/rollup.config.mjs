529ba9acc9e7ca9774a6af75fd17b8206e04d1b3
KiKiKi <chaika.design@gmail.com>
2022-08-05 16:21:25 +0900

    :bee: Remove `console.log` - Strip plugin don't work in `output.plugins`

--- rollup/rollup.config.mjs
+++ rollup/rollup.config.mjs
@@ -3,7 +3,10 @@ import { babel } from '@rollup/plugin-babel';
 // not working with ESM and CJS
 // commonjs() プラグインを使うと jQuery がうまくバンドルできない
 import commonjs from '@rollup/plugin-commonjs';
+// minify
 import { terser } from 'rollup-plugin-terser';
+// remove console.log
+import strip from '@rollup/plugin-strip';
 
 const config = {
   // entrypoint
@@ -17,13 +20,21 @@ const config = {
     {
       file: './build/dist.min.js',
       format: 'iife',
-      plugins: [terser()],
+      plugins: [
+        // Strip plugin don't work in output.plugins!
+        // The "transform" hook used by the output plugin strip is a build time hook and will not be run for that plugin. Either this plugin cannot be used as an output plugin, or it should have an option to configure it as an output plugin.
+        strip({
+          labels: ['unittest'],
+        }),
+        terser(),
+      ],
     },
   ],
   plugins: [
     // commonjs(),
     nodeResolve(),
     babel({ babelHelpers: 'bundled' }),
+    ,
   ],
 };
 
